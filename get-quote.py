8deb9e98779724219059d0a597e9590953a6ec52
theabhinavanand <abhinav.11710471@lpu.in>
2020-12-30 20:40:18 +0530

    adding comment

--- get-quote.py
+++ get-quote.py
@@ -1,17 +1,20 @@
+#importing random for generating random numbers
 import random
 
 def Primary():
-
- 
-  print("Keep it logically awesome.")
-
+#opening files
   f = open("quotes.txt")
+#storing lines
   quotes = f.readlines()
+#now close the file
   f.close()
 
+#last is storing postition of last quote
   last = len(quotes) - 1
+#using randint to pick random line 
   rnd = random.randint(0,last)
   print(quotes[rnd])
-
+#python entrypoint
 if __name__== "__main__":
+#calling function
   Primary()
